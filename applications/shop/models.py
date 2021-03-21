8f1fab57852e2ee758654bd88b5d8d2bddd9fcdc
Paul Stetsenko <28633195+p-stets@users.noreply.github.com>
2021-03-22 00:53:50 +0200

    models finish + validations + properties

--- applications/shop/models.py
+++ applications/shop/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, AbstractBaseUser, UserManager
+from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
+from main_config.settings import MEDIA_ROOT
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
@@ -25,36 +28,49 @@ class ProductMovementMixin(DateMixin):
         income and outcome
     '''
 
+    RELEVANT = 1
+    PENDING_CANCELLATION = 2
+    CANCELLED = 3
+
+    STATUSES = (
+        (RELEVANT, 'Relevant'),
+        (PENDING_CANCELLATION, 'Pending Cancellation'),
+        (CANCELLED, 'Cancelled')
+    )
+
     class Meta:
         abstract = True
 
+    user = models.ForeignKey('User', on_delete=models.PROTECT, verbose_name='Author')
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    status = models.PositiveIntegerField(choices=STATUSES, default=1)
+
+    def __str__(self):
+        return f'{self.id} - {self.product}'
 
 
 class User(AbstractUser, DateMixin):
 
     '''
+    A User.
     '''
 
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['username', 'wallet']
     USERNAME_FIELD = 'email'
     username = models.CharField(max_length=50, verbose_name='Nick-name')
     email = models.EmailField(unique=True)
+    base_wallet = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Base wallet amount')
 
-
-# class Admin(User):
-
-#     pass
-
-
-# class Customer(User):
-
-#     '''
-#     Platform customer
-#     '''
-
-#     credit_limit = models.PositiveIntegerField(verbose_name='Customer credit limit')
+    @property
+    def current_money(self):
+        status_actual = models.Q(status=1)
+        status_pending = models.Q(status=2)
+        spent_amount_queryset = ProductSell.objects.filter(status_actual | status_pending, user=self.id)
+        bought_total = 0
+        for obj in spent_amount_queryset:
+            bought_total += obj.quantity * obj.product.price
+        return self.base_wallet - bought_total
 
 
 class Product(DateMixin):
@@ -64,20 +80,51 @@ class Product(DateMixin):
     '''
 
     name = models.CharField(verbose_name='Product name', unique=True, max_length=120)
-    price = models.PositiveIntegerField(name='Product price')
-    image = models.ImageField(verbose_name='Product Image', upload_to='uploads/')
+    description = models.TextField(verbose_name='Product description', max_length=2000, null=True, blank=True)
+    price = models.DecimalField(verbose_name='Product price', max_digits=10,
+                                decimal_places=2, validators=[MinValueValidator(0.01)])
+    image = models.ImageField(verbose_name='Product Image', upload_to='products/')
 
     @property
-    def absolute_path(self):
-        return f'{self.image.name}'
+    def current_stock(self):
+        # Set statuses actual and pending cancellation
+        status_actual = models.Q(status=1)
+        status_pending = models.Q(status=2)
+        # Get count of actual product quantity
+        stock_queryset = ProductIncome.objects.filter(status_actual | status_pending, product__id=self.id)
+        product_total_quantity = 0
+        for item in stock_queryset:
+            product_total_quantity += item.quantity
+
+        # Get count of bought products
+        bought_queryset = ProductSell.objects.filter(status_actual | status_pending, product__id=self.id)
+        bought_product_count = 0
+        for item in bought_queryset:
+            bought_product_count += item.quantity
+
+        return product_total_quantity - bought_product_count
+
+    @property
+    def image_path(self):
+        return f'{MEDIA_ROOT}{self.image.name}'
 
     def __str__(self):
-        return f'{self.id} - {self.name}'
+        return f'Product - {self.id} - {self.name}'
 
 
 class ProductIncome(ProductMovementMixin):
     pass
 
 
-class ProductReturn(ProductMovementMixin):
-    pass
+class ProductSell(ProductMovementMixin):
+    def clean(self):
+        try:
+            product = Product.objects.get(id=self.product_id)
+            user = User.objects.get(id=self.user_id)
+            if product.current_stock - self.quantity <= 0:
+                raise ValidationError({'quantity': 'Not enough in stock'})
+            if user.current_money - (product.price * self.quantity) < 0:
+                raise ValidationError({'user': 'User doesn\'t have enough money'})
+        except (Product.DoesNotExist, User.DoesNotExist):
+            product = None
+            user = None
--- main_config/settings.py
+++ main_config/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+MEDIA_ROOT = '/home/stets/learn.python/django/django-module/uploads/'
 
 # Application definition
 
